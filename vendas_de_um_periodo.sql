b41699606b3e7b66dcae6f1a5cac7ade3b081dd8
Italo Marques <156384611+ItaloHenriqueMarquesCampos@users.noreply.github.com>
2025-02-03 13:27:47 -0300

    Lista as vendas realizadas à partir de uma data
    Lista as vendas realizadas à partir de uma data por parceiro e tipo de equipe do parceiro.
    Isso ajuda muito ao fazer um levantamento de vendas por equipe.
    Consta na query o valor de contrato do curso.

---
+++ vendas_de_um_periodo.sql
@@ -0,0 +1,22 @@
+SELECT
+    -- Dados do aluno
+    c.nome_fin,
+    c.curso,
+    c.nivel,
+    c.taxa_matricula,
+    c.parcelas,
+    c.valor_parcela,
+
+    -- Dados de quem matriculou
+    c.parceiro_id,
+    p.id_equipe,
+    p.tipo,
+    p.nome
+
+    FROM curso_aluno c
+    INNER JOIN
+        parceiros p ON c.parceiro_id = p.id
+
+WHERE datahora> "2024-12-31 23:59:00"
+
+-- AND c.parceiro_id = 25745;
